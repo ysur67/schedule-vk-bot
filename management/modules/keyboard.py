f09257bbe3ca63646629435cf2598da4adccc829
ysur67 <rus.muratow@gmail.com>
2020-12-08 15:20:24 +0300

    Made it's own file for Keyboard class

---
+++ management/modules/keyboard.py
@@ -0,0 +1,95 @@
+from .Table import Table
+
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+
+class Keyboard:
+    settings = dict(one_time=False, inline=False)
+
+    table_manager = Table()
+
+    def __init__(self, keyboard_type, message=""):
+        if keyboard_type == "BEGIN":
+            self.keyboard = self.Begin_Keyboard()
+        elif keyboard_type == "COURSES":
+            self.keyboard = self.Courses_Keyboard()
+        elif keyboard_type == "GROUPS":
+            self.keyboard = self.Groups_Keyboard(message)
+        elif keyboard_type == "TIMETABLE":
+            self.keyboard = self.TimeTable_Keyboard()
+        elif keyboard_type == "SETTINGS":
+            self.keyboard = self.Settings_Keyboard()
+    
+    def Begin_Keyboard(self):
+        keyboard = VkKeyboard(**self.settings)
+        keyboard.add_button('Начать')
+        return keyboard
+
+    def Courses_Keyboard(self):
+        keyboard = VkKeyboard(**self.settings)
+        keyboard.add_button('1 курс')
+        keyboard.add_line()
+        keyboard.add_button('2 курс')
+        keyboard.add_line()
+        keyboard.add_button('3 курс')
+        keyboard.add_line()
+        keyboard.add_button('4 курс')
+        keyboard.add_line()
+        keyboard.add_button(label='Изменить расписание')
+        keyboard.add_line()
+        keyboard.add_button(label='Настройка уведомлений')
+        return keyboard
+
+    def Groups_Keyboard(self, answer):
+        keyboard_ = VkKeyboard(**self.settings)
+        group_names_by_course = []
+        if (answer == "1КУРС") or answer=="1":
+            group_names_by_course = self.table_manager.groupNamesByCourse("C5", "K5")
+        elif answer=="2КУРС" or answer=="2":
+            group_names_by_course = self.table_manager.groupNamesByCourse("L5", "S5")
+        elif answer=="3КУРС" or answer=="3":
+            group_names_by_course = self.table_manager.groupNamesByCourse("T5", "AB5")
+        elif answer=="4КУРС" or answer=="4":
+            group_names_by_course = self.table_manager.groupNamesByCourse("AC5", "AD5")
+        i = 0
+        while i<len(group_names_by_course):
+            keyboard_.add_button(group_names_by_course[i])
+            keyboard_.add_line()
+            i+=1
+        keyboard_.add_button(label= "Назад", color=VkKeyboardColor.PRIMARY)
+        return keyboard_
+
+    def TimeTable_Keyboard(self):
+        keyboard_ = VkKeyboard(**self.settings)
+        time_tables = []
+        i = 0
+        dates = self.table_manager.dates
+        for key in dates:
+            time_tables.append(self.__proper_text_for_button(str(key)))
+        while i < len(time_tables):
+            keyboard_.add_button(time_tables[i])
+            keyboard_.add_line()
+            i+=1
+        keyboard_.add_button(label="Назад", color=VkKeyboardColor.PRIMARY)
+        return keyboard_
+
+    def __proper_text_for_button(self, text):
+        return text[0:10] + " " + text[27:]
+
+    def Settings_Keyboard(self):
+        keyboard_ = VkKeyboard(**self.settings)
+        keyboard_.add_button(
+            label="Получать уведомления", 
+            color=VkKeyboardColor.POSITIVE,
+            )
+        keyboard_.add_line()
+        keyboard_.add_button(
+            label="Не получать уведомлений", 
+            color=VkKeyboardColor.NEGATIVE,
+            )
+        keyboard_.add_line()
+        keyboard_.add_button(label="Назад", color=VkKeyboardColor.SECONDARY)
+        return keyboard_
+
+    def get_keyboard(self):
+        return self.keyboard
+        
